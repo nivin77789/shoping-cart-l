056f35f5b91fd325c368ee8a30ebd6b2e97a4b9a
Nivin Mathew S <64590350+nivin77789@users.noreply.github.com>
2023-08-20 01:03:09 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,16 +3,8 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="cont">
-        <a href="https://github.com/bmilcs/odin-shopping-cart">
-          <img
-            alt="giti"
-            src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png"
-          ></img>
-          BMILCS
-        </a>
-      </div>
+    <div>
+      
     </div>
   );
 }
